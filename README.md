f6833063812d416b1ae0f1794ce6c719fd6b06dd
Michael Kemna <m.j.kemna@student.tudelft.nl>
2019-06-01 11:39:41 +0200

    Add kubernetes client

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # kubernetes-job-deployment-automation
 Demo app to try out kubernetes python client to automate deployment of jobs
+
+The jobs are doing nothing useful (printing out to shell), they are placeholders just to prove the principle.
+You can add your own image and command args in the job template to make it useful.
+
+It is useful to have another command line watching your pods on the background:   
+`watch -n 5 kubectl get pods`
+
+You can verify the output of your container with:   
+`kubectl logs <<POD NAME>>`
+
+Completed jobs will not be automatically deleted. This requires the TTLAFterFinished, which is still in alpha phase.
+You can delete the batch of jobs manually with:   
+`kubectl delete jobs.batch <<BATCH NAME>>`   
+or, if you are living on the edge:      
+`kubectl delete jobs.batch --all`
+
+
+## Requirements
+You have a kubernetes cluster running somewhere with valid kubeconfig credentials in `~/.kube/config`.
+
+
+## Tools used
+**Jinja2 templating**: http://jinja.pocoo.org/docs/2.10/api/   
+This is used to dynamically inject variables into the yaml file
+
+**kubernetes python client**: https://github.com/kubernetes-client/python/blob/master/kubernetes/   
+This is used to access the kubernetes API from within python
--- app/deploy.py
+++ app/deploy.py
@@ -1,11 +1,60 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
+from kubernetes import client, config
+from kubernetes.client import V1Job
+from kubernetes.client.rest import ApiException
+import yaml
 
+# The environment of jinja to retrieve jinja templates from filesystem
 env = Environment(
-    loader=PackageLoader('app', 'templates'),
-    autoescape=select_autoescape(['yaml'])
+    loader=PackageLoader(package_name='app', package_path='templates'),
+    autoescape=select_autoescape(enabled_extensions=['yaml'])
 )
 
-template = env.get_template('job.yaml')
+# Retrieve the template of the job that we will be running
+job_template = env.get_template('job.yaml')
+
+# Configure the kubernetes client to connect to our cluster
+# The default config expects a config file with valid credentials in ~/.kube/
+config.load_kube_config()
+# You can also request the access from within the cluster
+# it works only if this script is run by K8s as a POD
+# config.load_incluster_config()
+
+
+def deploy_job_batch(job_id: str, project_env: str, completions: int, parallelism: int) -> V1Job:
+    """
+    Deploy a batch of jobs on the kubernetes cluster.
+
+    This is based on https://github.com/kubernetes-client/python/blob/master/kubernetes/docs/BatchV1Api.md
+
+    :param job_id: the name of the job, this needs to be unique.
+    :param project_env: the environment in which the project is running. This will be used as the image tag of the worker.
+    :param completions: the amount of jobs within this batch
+    :param parallelism: the amount of jobs that is allowed to run simultaneously.
+    :return: The API response
+    """
+    # this is the rendered job
+    job: V1Job = yaml.safe_load(job_template.render(
+        jobID=job_id,
+        tag=project_env,
+        completions=completions,
+        parallelism=parallelism))
+
+    # this is the RESTful API instance that we can post requests to
+    api_client = client.BatchV1Api()
+
+    try:
+        api_response = api_client.create_namespaced_job(namespace="default", body=job)
+        return api_response
+    except ApiException as e:
+        print("Exception when calling BatchV1Api->create_namespaced_job: %s\n" % e)
+
 
 if __name__ == "__main__":
-    print(template.render(item="two"))
+    JOB_ID: str = "seven"
+    PROJECT_ENV: str = "latest"
+    COMPLETIONS: int = 4
+    PARALLELISM: int = 2
+
+    deploy_job_batch(job_id=JOB_ID, project_env=PROJECT_ENV, completions=COMPLETIONS, parallelism=PARALLELISM)
+
